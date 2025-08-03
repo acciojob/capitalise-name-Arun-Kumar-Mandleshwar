5dc8196c569e365871a3b414f081673fe273f300
Arun  Kumar <161482514+Arun-Kumar-Mandleshwar@users.noreply.github.com>
2025-08-04 01:40:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+document.getElementById("fname").addEventListener("focusout", myFunction);
+function myfunction() {
+let textField=document.getElementById('fname');
+let content=textField.value;
+content=content.toUpperCase();
+textField.value=content;
+}
